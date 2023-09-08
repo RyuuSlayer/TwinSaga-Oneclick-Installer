0055f077d2fd41271ef92fbb4e88bacf4c13e839
RyuuM3gum1n <33106127+RyuuSlayer@users.noreply.github.com>
2023-09-08 16:57:54 +0200

    Update tsinstall.sh

--- tsinstall.sh
+++ tsinstall.sh
@@ -109,7 +109,7 @@ if [ "$TSVERSION" = 1 ] ; then
 	sed -i "s/\x3d\xc0\xa8\xb2/\x3d$PATCHIP/g" "ZoneServer/ZoneServer"
 
 	# Data folder
-	wget --no-check-certificate --load-cookies "/tmp/cookies.txt" "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$DATAFOLDER" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$DATAFOLDER" -O "Data.zip" && rm -rf "/tmp/cookies.txt"
+	wget --no-check-certificate --load-cookies "/tmp/cookies.txt" "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$DATAFOLDER" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$DATAFOLDER" -O "data.zip" && rm -rf "/tmp/cookies.txt"
 	unzip "data.zip" -d "data"
 	rm -f "data.zip"
 	
